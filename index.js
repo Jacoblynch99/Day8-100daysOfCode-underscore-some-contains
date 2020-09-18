040bf3389201c4d28ab051cc52231fc33c41304c
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-18 10:34:11 -0500

    contains done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const isSquare = (num) => {
   return num > 0 && Math.sqrt(num) % 1 === 0
 }
 
-const square = _.some(numArr, isSquare)
+const square = _.some(numArr, isSquare) // returns true if even one element in the array fits the criteria established by the function
+
+const foundNum = _.contains(numArr, 82) // returns true only if the given value is in the array
 
 console.log(square)
+console.log(foundNum)
